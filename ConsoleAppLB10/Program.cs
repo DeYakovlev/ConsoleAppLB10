826d7d8b12e11a246e89805b345fdad924f445d2
d.Yakovlev <d.Yakovlev@WS43>
2026-09-18 11:20:44 +0300

    Task 5 Update

--- ConsoleAppLB10/Program.cs
+++ ConsoleAppLB10/Program.cs
@@ -13,7 +13,9 @@ namespace ConsoleAppLB10
         {
             //Task1();
             //Task2();
-            Task3();
+            //Task3();
+            //Task4();
+            Task5();
         }
 
         public static void Task1() 
@@ -139,6 +141,52 @@ namespace ConsoleAppLB10
 
 
         }
+
+        public static void Task4() 
+        {
+            List<Person> people = new List<Person>() 
+            {
+                new Person("Жмышенко", DateTime.Parse("15.02.1985"), "Специалист", 100, "ООО Мэгаюлюль"),
+                new Person("Валакас", DateTime.Parse("17.02.1985"), "Грузчик", 100, "ООО Мэгаюлюль"),
+                new Person("Каруман", DateTime.Parse("25.03.1975"), "Начальник", 100, "Пожилая ветка сакуры"),
+                new Person("Александр", DateTime.Parse("05.10.1985"), "Специалист", 100, "Пожилая ветка сакуры"),
+                new Person("Якобсон", DateTime.Parse("15.11.1985"), "Специалист", 100, "Пожилая ветка сакуры"),
+
+            };
+
+            List<ShortPerson> res = GetShortPersonByYearAndCompany(people, 1985, "Пожилая ветка сакуры");
+            //Console.WriteLine(res);
+            foreach (var person in res)
+            {
+                Console.WriteLine(person);
+            }
+            
+
+        }
+        public static void Task5() 
+        {
+            List<Person2> people = new List<Person2>()
+            {
+                new Person2("Жмышенко", 1985, "Специалист", 3000, new Company("ООО Мэгаюлюль", 2000)),
+                new Person2("Валакас", 1985, "Грузчик", 2500, new Company("ООО Мэгаюлюль", 2000)),
+                new Person2("Каруман", 1975, "Начальник", 7000,new Company("Пожилая ветка сакуры", 2010)),
+                new Person2("Александр", 1985, "Специалист", 5500, new Company("Пожилая ветка сакуры", 2010)),
+                new Person2("Якобсон", 1985, "Специалист", 5500,new Company("Пожилая ветка сакуры", 2010)),
+
+            };
+
+            var res = from p in people group p by p.Company.Name into g select new {Company = g.Key, TotalSallary = g.Sum(p => p.Selary) };
+
+            foreach (var item in res) 
+            {
+                Console.WriteLine(item);
+            }
+
+            //Console.WriteLine(res);
+
+
+
+        }
         public static void ShowArray(int[] array) 
         {
             int i = 0;
@@ -156,5 +204,84 @@ namespace ConsoleAppLB10
                 }
             }
         }
+
+        public static List<ShortPerson> GetShortPersonByYearAndCompany(List<Person> people, int year, string company)
+        {
+           return people.Where(p => p.BirthDate.Year == year && p.Company == company).Select(p=> new ShortPerson(p.Name, p.BirthDate, p.Company)).ToList();
+        }
+
+      public  class Person 
+        {
+            public string Name { get; set; }
+            public DateTime BirthDate { get; set; }
+            public string Position { get; set; }
+            public decimal Selary { get; set; }
+            public string Company { get; set; }
+
+            public Person (string name, DateTime birthDate, string position, decimal selary, string company) 
+            {
+                Name = name;
+                BirthDate = birthDate;
+                Position = position;
+                Selary = selary;
+                Company = company;
+            }
+        }
+
+        public class Company 
+        {
+            public string Name { get; set; }
+            public int FoundedYear { get; set; }
+
+            public Company(string name, int year) 
+            {
+                Name = name;
+                FoundedYear = year;
+
+            }
+
+            public override string ToString()
+            {
+                return $"{Name} (основана в {FoundedYear})";
+            }
+        }
+        
+        public class Person2
+        {
+            public string Name { get; set; }
+            public int BirthYear { get; set; }
+            public string Position { get; set; }
+            public decimal Selary { get; set; }
+            public Company Company { get; set; }
+
+            public Person2(string name, int birthDate, string position, decimal selary, Company company)
+            {
+                Name = name;
+                BirthYear = birthDate;
+                Position = position;
+                Selary = selary;
+                Company = company;
+            }
+        }
+
+        public class ShortPerson 
+        {
+            public string Name {  set; get; }
+            public DateTime BirthDate { set; get; }
+            public string Company {  set; get; }
+
+            public ShortPerson(string name, DateTime birthDate, string company) 
+            {
+                Name = name;
+                BirthDate = birthDate;
+                Company = company;
+            }
+
+            public override string ToString()
+            {
+                return $"{Name}, дата рождения: {BirthDate:dd.MM.yyyy}, компания: {Company}";
+            }
+        }
+
     }
 }
